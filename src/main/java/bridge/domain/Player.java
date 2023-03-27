9d73846ef2e176b6fd481be5778978ca1de8de0a
onegqueen <rkdwldnjs30@naver.com>
2023-03-27 20:41:22 +0900

    Feat: Player 클래스에 이동칸 입력에 대한 예외처리

--- src/main/java/bridge/domain/Player.java
+++ src/main/java/bridge/domain/Player.java
@@ -1,28 +1,39 @@
 package bridge.domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Player {
-    List<String> history;
+    public static final String MOVABLE = "UD";
+    private List<String> record;
     int tryNumber;
 
     public Player() {
         this.tryNumber = 0;
     }
 
-    public void makeHistory(String moving){
-        this.history.add(moving);
+    public void makeRecord(String moving) {
+        if(!MOVABLE.contains(moving)){
+            throw new IllegalArgumentException("[Error]");
+        }
+        this.record.add(moving);
     }
+
     public void updateTryNumber() {
         this.tryNumber += 1;
     }
 
-    public void setTryNumber() {
-        this.tryNumber = 0;
+    public void resetRecord() {
+        this.record = Arrays.asList();
     }
 
     public int getTryNumber() {
         return tryNumber;
     }
+
+    public List<String> getRecord() {
+        return record;
+    }
+
 }
