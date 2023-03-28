ba3dcc84aa7bfa666ec81304d648f0b72190acc8
onegqueen <rkdwldnjs30@naver.com>
2023-03-29 00:25:31 +0900

    Feat: 게임 재시도 기능 구현

--- src/main/java/bridge/controller/BridgeGame.java
+++ src/main/java/bridge/controller/BridgeGame.java
@@ -15,7 +15,7 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    public static final int START_STAGE = 0;
+    public static final int START_STAGE = 1;
     public static final String MOVE_INPUT_VALUE = "UD";
     public static final String MOVEMENT_INPUT_VALUE1 = MOVE_INPUT_VALUE;
     public static final String MOVEMENT_INPUT_VALUE = MOVEMENT_INPUT_VALUE1;
@@ -29,32 +29,53 @@ public class BridgeGame {
     private int lastStage;
 
     public void run() {
+        OutputView.printStartMessege();
         lastStage = InputView.readBridgeSize();
         buildBridge(lastStage);
+        boolean success = true;
 
-        boolean success = move(InputView.readMoving(),stage);
-        showResult(player.getRecord(),success);
-    }
-    private void showResult(List<String> playerRecord,boolean success){
-        List<String>downBlock = new ArrayList<>(playerRecord);
-        List<String>upBlock = new ArrayList<>(playerRecord);
-        if(!success&&playerRecord.get(playerRecord.size()-1).equals("D")){
-            downBlock.set(playerRecord.size()-1,"X");
+        while (success) {
+            success = move(InputView.readMoving(), stage);
+            OutputView.printMap(showResult(player.getRecord(), success));
+            if (success && stage > lastStage) {
+                break;
+            }
+            if (!success && InputView.readGameCommand().equals("Q")){
+                break;
+            }
+            if (!success){
+                success = true;
+                retry();
+            }
         }
-        if(!success&&playerRecord.get(playerRecord.size()-1).equals("U")){
-            upBlock.set(playerRecord.size()-1,"X");
-        }
-        OutputView.printMap(downBlock,upBlock);
+
+        OutputView.printResult(showResult(player.getRecord(),success),success, player.getTryNumber());
     }
 
-    private void buildBridge(int size){
+    private List<List<String>> showResult(List<String> playerRecord, boolean success) {
+        List<List<String>> result = new ArrayList<>();
+        List<String> downBlock = new ArrayList<>(playerRecord);
+        List<String> upBlock = new ArrayList<>(playerRecord);
+        if (!success && playerRecord.get(playerRecord.size() - 1).equals("D")) {
+            downBlock.set(playerRecord.size() - 1, "X");
+        }
+        if (!success && playerRecord.get(playerRecord.size() - 1).equals("U")) {
+            upBlock.set(playerRecord.size() - 1, "X");
+        }
+        result.add(upBlock);
+        result.add(downBlock);
+        return result;
+    }
+
+    private void buildBridge(int size) {
         bridge = new Bridge(size);
         bridge.build(bridgeMaker.makeBridge(size));
     }
 
-    public boolean move(String playerMoving,int stage){
+    public boolean move(String playerMoving, int stage) {
+        this.stage = stage + 1;
         player.makeRecord(playerMoving);
-        return bridge.isCorrect(stage,playerMoving);
+        return bridge.isCorrect(stage, playerMoving);
     }
 
     /**
@@ -63,6 +84,8 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
-
+        player.updateTryNumber();
+        player.resetRecord();
+        this.stage = START_STAGE;
     }
 }
