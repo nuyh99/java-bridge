46e4cfc591979f7a6cb2973ab32038733ae4ff0d
onegqueen <rkdwldnjs30@naver.com>
2023-04-10 23:39:26 +0900

    Feat: 개행줄 및 주석 삭제

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,15 +3,12 @@ package bridge;
 import bridge.controller.BridgeGame;
 
 public class Application {
-
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         try{
             BridgeGame bridgeGame = new BridgeGame();
             bridgeGame.run();
         }catch (IllegalArgumentException e){
             System.out.println(e.getMessage());
         }
-
     }
 }
