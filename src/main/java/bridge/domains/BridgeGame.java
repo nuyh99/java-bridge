3f6071ae1c0224e1b5d2341d3a23dc798fd27e69
howudong <53307093+howudong@users.noreply.github.com>
2023-07-14 00:20:03 +0900

    feat : retry를 할때마다 게임 횟수를 기록하는 기능 추가

--- src/main/java/bridge/domains/BridgeGame.java
+++ src/main/java/bridge/domains/BridgeGame.java
@@ -3,14 +3,27 @@ package bridge.domains;
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
-public class BridgeGame {
+final public class BridgeGame {
+    private final BridgeMap bridgeMap;
 
-    /**
+    private static int gameTryCount;
+
+    static {
+        gameTryCount = 1;
+    }
+
+    public BridgeGame(BridgeMap bridgeMap) {
+        this.bridgeMap = bridgeMap;
+    }
+    /*
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+
      */
-    public void move() {
+
+    public void move(String direction) {
+
     }
 
     /**
@@ -19,5 +32,10 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        gameTryCount++;
+    }
+
+    public void quit() {
+
     }
 }
