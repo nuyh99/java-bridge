79fd0ec14f7f318ee206211bc7388a3b9070db8e
howudong <53307093+howudong@users.noreply.github.com>
2023-07-15 13:04:08 +0900

    feat : 생성된 다리 정보와 움직인 경로, 해당 방향이 맞는지 확인하는 함수를 가진 BridgeMap 생성

--- src/main/java/bridge/domains/BridgeMap.java
+++ src/main/java/bridge/domains/BridgeMap.java
@@ -1,15 +1,34 @@
 package bridge.domains;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class BridgeMap {
+final public class BridgeMap {
     public final static String UP_SIDE = "U";
     public final static String DOWN_SIDE = "D";
 
-    private final List<String> bridgeMade;
+    private final List<String> map;
+    private final List<String> moveHistory;
 
-    public BridgeMap(List<String> bridgeMade){
-        this.bridgeMade = bridgeMade;
+    public BridgeMap(BridgeMaker bridgeMaker, int size) {
+
+        map = bridgeMaker.makeBridge(size);
+        moveHistory = new ArrayList<>();
     }
 
+    public boolean isCorrectWay(String direction) {
+        int nextPosition = moveHistory.size();
+
+        recordMoveHistory(direction);
+
+        return map.get(nextPosition).equals(direction);
+    }
+
+    private void recordMoveHistory(String direction) {
+        moveHistory.add(direction);
+    }
+
+    public void reset() {
+        moveHistory.clear();
+    }
 }
