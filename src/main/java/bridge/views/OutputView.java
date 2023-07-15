50fe0df0a5c353b899fb6246ad716686357fe400
howudong <53307093+howudong@users.noreply.github.com>
2023-07-15 13:01:22 +0900

    feat : 입력 메시지 출력 추가

--- src/main/java/bridge/views/OutputView.java
+++ src/main/java/bridge/views/OutputView.java
@@ -1,15 +1,30 @@
-package bridge;
+package bridge.views;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    static {
+        System.out.println("다리 건너기 게임을 시작합니다\n");
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
+     * <p>`
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
+    void printInputBridgeSizeText() {
+        System.out.println("다리의 길이를 입력해주세요\n");
+    }
+
+    void printInputMoveDirection() {
+        System.out.println("이동할 칸을 선택해쥇요. (위: U, 아래: D");
+    }
+
+    void printRestartQuestion() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요.");
+    }
+
     public void printMap() {
     }
 
