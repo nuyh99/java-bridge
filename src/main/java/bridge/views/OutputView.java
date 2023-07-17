f73e7d380a9758eaff24460a30e8b6e7e57db18e
howudong <53307093+howudong@users.noreply.github.com>
2023-07-18 01:32:06 +0900

    feat : 모든 종류의 출력문 함수 만듦

--- src/main/java/bridge/views/OutputView.java
+++ src/main/java/bridge/views/OutputView.java
@@ -15,31 +15,78 @@ public final class OutputView {
         System.out.println("다리 건너기 게임을 시작합니다\n");
     }
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>`
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     void printInputBridgeSizeText() {
         System.out.println("다리의 길이를 입력해주세요\n");
     }
 
     void printInputMoveDirection() {
-        System.out.println("이동할 칸을 선택해쥇요. (위: U, 아래: D");
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
     }
 
     void printRestartQuestion() {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요.");
     }
 
-    public void printMap() {
+    public void printMap(List<String> moveHistory, boolean isSuccess) {
+
+        String result = draw(moveHistory, isSuccess);
+        System.out.println(result);
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult() {
+    private String draw(List<String> moveHistory, boolean isSuccess) {
+        String upSideResult = drawSection(UP_COMMAND, moveHistory, isSuccess);
+        String downSideResult = drawSection(DOWN_COMMAND, moveHistory, isSuccess);
+
+        return (trimmedResult(upSideResult) + "\n" + trimmedResult(downSideResult));
+    }
+
+    private String trimmedResult(String result) {
+        StringBuilder trimmedPrint = new StringBuilder(result);
+        trimmedPrint.replace(0, 0, "[ ");
+        trimmedPrint.append(" ]");
+
+        return trimmedPrint.toString();
+    }
+
+    private String drawSection(String keyWord, List<String> moveHistory, boolean isSuccess) {
+        List<String> printWords = moveHistory.stream()
+                .map((e) -> getPrintWord(keyWord, e))
+                .collect(Collectors.toList());
+
+        convertLastWord(printWords, moveHistory.size() - 1, isSuccess);
+        return String.join(" | ", printWords);
+    }
+
+    private void convertLastWord(List<String> printWords, int length, boolean isSuccess) {
+        System.out.println(isSuccess);
+        if (!isSuccess && printWords.get(length).equals("O")) {
+            printWords.remove(length);
+            printWords.add("X");
+        }
+    }
+
+    private String getPrintWord(String keyWord, String e) {
+        if (keyWord.equals(e)) {
+            return "O";
+        }
+        return " ";
+    }
+
+    public void printResult(List<String> moveHistory, boolean isSuccess) {
+        System.out.println();
+        System.out.println("최종 게임 결과");
+        printMap(moveHistory, isSuccess);
+    }
+
+    public void printGameInformation(boolean isSuccess, int gameCount) {
+        System.out.println();
+
+        String gameSuccessText = "실패";
+        if (isSuccess) {
+            gameSuccessText = "성공";
+        }
+
+        System.out.println("게임 성공 여부: " + gameSuccessText);
+        System.out.println("총 시도한 횟수: " + gameCount);
     }
 }
