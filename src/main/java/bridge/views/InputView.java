3665384d2d99595d58c7837494f64e074b0489b8
howudong <53307093+howudong@users.noreply.github.com>
2023-07-18 01:32:47 +0900

    refactor: 예외 처리 및 예외발생시 해당 함수 재시작하는 것을 inputview에서 하도록 처리

--- src/main/java/bridge/views/InputView.java
+++ src/main/java/bridge/views/InputView.java
@@ -11,10 +11,10 @@ import java.util.List;
 
 public final class InputView {
 
-    private static final String UP_COMMAND = "U";
-    private static final String DOWN_COMMAND = "D";
-    private static final String QUIT_COMMAND = "Q";
-    private static final String RESTART_COMMAND = "R";
+    static final String UP_COMMAND = "U";
+    static final String DOWN_COMMAND = "D";
+    public static final String QUIT_COMMAND = "Q";
+    public static final String RESTART_COMMAND = "R";
 
     private final OutputView outputView;
 
@@ -24,24 +24,40 @@ public final class InputView {
 
     public int readBridgeSize() {
         outputView.printInputBridgeSizeText();
-
         String readBridgeSize = Console.readLine();
-        InputChecker.validateNumeric(this, readBridgeSize);
 
+        try {
+            InputChecker.validateNumeric(this, readBridgeSize);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            readBridgeSize = String.valueOf(readBridgeSize());
+        }
         return Integer.parseInt(readBridgeSize);
     }
 
     public String readMoving() {
         outputView.printInputMoveDirection();
         String readDirection = Console.readLine();
-        InputChecker.validateCommand(List.of(UP_COMMAND, DOWN_COMMAND), readDirection);
+
+        try {
+            InputChecker.validateCommand(List.of(UP_COMMAND, DOWN_COMMAND), readDirection);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            readDirection = readMoving();
+        }
         return readDirection;
     }
 
     public String readGameCommand() {
         outputView.printRestartQuestion();
         String readCommand = Console.readLine();
-        InputChecker.validateCommand(List.of(QUIT_COMMAND, RESTART_COMMAND), readCommand);
+
+        try {
+            InputChecker.validateCommand(List.of(QUIT_COMMAND, RESTART_COMMAND), readCommand);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            readCommand = readGameCommand();
+        }
         return readCommand;
     }
 }
