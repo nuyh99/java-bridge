c663109d364edd0e578786d473f2595338ab3408
howudong <53307093+howudong@users.noreply.github.com>
2023-07-31 23:48:09 +0900

    feat: dto를 통해 BridgeGame 객체를 만들 수 있도록 생성자 오버로딩

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -4,26 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public final class BridgeGame {
-
     public final static String UP_SIDE = "U";
     public final static String DOWN_SIDE = "D";
-    private static int gameTryCount;
+
+    private int gameTryCount;
     private final List<String> moveLogs;
     private final List<String> map;
 
     public BridgeGame(List<String> map) {
         this.map = map;
         moveLogs = new ArrayList<>();
-    }
-
-    static {
         gameTryCount = 1;
     }
 
+    public BridgeGame(List<String> map, List<String> moveLogs, int gameTryCount) {
+        this.map = map;
+        this.moveLogs = moveLogs;
+        this.gameTryCount = gameTryCount;
+    }
+
     public boolean move(String direction) {
-        int nextPosition = moveLogs.size();
+        if (moveLogs.size() >= map.size()) {
+            return false;
+        }
         moveLogs.add(direction);
-        return map.get(nextPosition).equals(direction);
+        return true;
     }
 
     public void reset() {
@@ -42,5 +47,9 @@ public final class BridgeGame {
 
     public List<String> getMoveLogs() {
         return moveLogs;
+    }
+
+    public List<String> getMap() {
+        return map;
     }
 }
