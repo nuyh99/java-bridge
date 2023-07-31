21e0adcbce24ffb459f1ff5551366d50b64fabaa
howudong <53307093+howudong@users.noreply.github.com>
2023-07-31 23:49:54 +0900

    fix: MoveLogs 리스트에 추가가 불가능하여 unmodifiableList를 삭제

--- src/main/java/bridge/dto/GameDto.java
+++ src/main/java/bridge/dto/GameDto.java
@@ -2,19 +2,19 @@ package bridge.dto;
 
 import bridge.domain.BridgeGame;
 
-import java.util.Collections;
 import java.util.List;
 
-public final class GameInformationDto {
+public final class GameDto {
 
     private final BridgeGame bridgeGame;
 
-    public GameInformationDto(BridgeGame bridgeGame) {
+    
+    public GameDto(BridgeGame bridgeGame) {
         this.bridgeGame = bridgeGame;
     }
 
     public List<String> getMoveLogs() {
-        return Collections.unmodifiableList(bridgeGame.getMoveLogs());
+        return bridgeGame.getMoveLogs();
     }
 
     public int getGameTryCount() {
