0e478e2033975a938ae2be7019bf8d12fe58cdbe
howudong <53307093+howudong@users.noreply.github.com>
2023-07-31 23:55:32 +0900

    refactor: dto와 model 간의 변환을 포함한 dto를 사용하도록 코드 리팩토링
    - step -> stepNext로 이름 변경하고 GameDto를 반환
    - BridgeGame에 있던 isMatch 이동
    - makeBridgeMap -> 다리를 만드는 함수 여기로 이동

--- src/main/java/bridge/service/BridgeService.java
+++ src/main/java/bridge/service/BridgeService.java
@@ -1,28 +1,46 @@
 package bridge.service;
 
 import bridge.domain.BridgeGame;
-import bridge.dto.GameInformationDto;
+import bridge.domain.BridgeMaker;
+import bridge.domain.BridgeRandomNumberGenerator;
+import bridge.dto.GameDto;
+import bridge.dto.MapDto;
+
+import java.util.List;
 
 public final class BridgeService {
-    private final BridgeGame bridgeGame;
 
-    public BridgeService(BridgeGame bridgeGame) {
-        this.bridgeGame = bridgeGame;
+    public MapDto makeBridgeMap(int mapSize) {
+        List<String> map = new BridgeMaker(
+                new BridgeRandomNumberGenerator()).makeBridge(mapSize);
+
+        return new MapDto(new BridgeGame(map));
     }
 
-    public GameInformationDto checkBridge() {
-        return new GameInformationDto(bridgeGame);
+    public GameDto stepNext(GameDto gameDto, MapDto mapDto, String direction) {
+        BridgeGame bridgeGame =
+                new BridgeGame(mapDto.getMap(), gameDto.getMoveLogs(), gameDto.getGameTryCount());
+
+        if (bridgeGame.move(direction)) {
+            return new GameDto(bridgeGame);
+        }
+        return new GameDto(new BridgeGame(mapDto.getMap()));
     }
 
-    public boolean step(String direction) {
-        return bridgeGame.move(direction);
+    public boolean isMatch(MapDto mapDto, GameDto gameDto) {
+        List<String> moveLogs = gameDto.getMoveLogs();
+        int currentPoint = moveLogs.size() - 1;
+
+        if (currentPoint < 0) return true;
+        return moveLogs.get(currentPoint).equals(mapDto.getMap().get(currentPoint));
     }
 
-    public boolean isClear() {
-        return bridgeGame.isGameSuccess();
-    }
+    public GameDto resetGame(GameDto gameDto, MapDto mapDto) {
+        BridgeGame bridgeGame =
+                new BridgeGame(mapDto.getMap(), gameDto.getMoveLogs(), gameDto.getGameTryCount());
 
-    public void restartGame() {
         bridgeGame.reset();
+
+        return new GameDto(bridgeGame);
     }
 }
